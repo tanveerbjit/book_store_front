197259acb54d273f6e97ca58d431d2a7b9f2064a
tanveer ahmed <tanveer.ahmed@bjitacademy.com>
2023-10-09 17:05:44 +0600

    store commit
    new store created

---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import categoryReducer from "./slices/home/filter/categorySlice";
+import authorReducer from "./slices/home/filter/authorSlice";
+import publisherReducer from "./slices/home/filter/publisherSlice";
+import ratingReducer from "./slices/home/filter/ratingSlice";
+import priceReducer from "./slices/home/filter/priceSlice";
+import sortReducer from "./slices/home/sorting/sortingSlice";
+import itemNumberReducer from "./slices/home/number_of_item/itemNumberSlice";
+import searchReducer from "./slices/home/search/searchingSlice";
+import pageReducer from "./slices/home/pagination/paginationSlice";
+import userProfileReducer from "./slices/user/profile/userProfileSlice";
+
+
+const store = configureStore({
+  reducer: {
+    category: categoryReducer,
+    author: authorReducer,
+    publisher: publisherReducer,
+    rating: ratingReducer,
+    price: priceReducer,
+    sort: sortReducer,
+    itemNumber: itemNumberReducer,
+    search: searchReducer,
+    page: pageReducer,
+    userProfile: userProfileReducer,
+  },
+});
+
+export default store;
